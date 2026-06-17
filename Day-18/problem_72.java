4f09f9c85970dd9b525b61f7beac7c6201e81460
Arjav-Jain05 <arjav.jain.one@gmail.com>
2026-06-18 00:03:37 +0530

    Day-18 uploaded

---
+++ Day-18/problem_72.java
@@ -0,0 +1,28 @@
+// Write a program to Sort Array in Descending Order.
+
+import java.util.Scanner;
+
+public class problem_72 {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter size of array: ");
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            System.out.println("Enter elements:");
+            for (int i = 0; i < n; i++)
+                arr[i] = sc.nextInt();
+            for (int i = 0; i < n - 1; i++) {
+                for (int j = 0; j < n - 1 - i; j++) {
+                    if (arr[j] < arr[j + 1]) {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+            System.out.println("Descending Order:");
+            for (int num : arr)
+                System.out.print(num + " ");
+        }
+    }
+}
